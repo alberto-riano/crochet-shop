3104456fca13422e5194b84b647ae1b3048743c4
Alberto Riaño Gonzalez <az_alberto.riano.ext@W4ALL.SERVICES>
2026-06-11 20:42:23 +0200

    Add fsc page

--- crochet_shop/urls.py
+++ crochet_shop/urls.py
@@ -2,12 +2,19 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from pathlib import Path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('shop/', include('apps.products.urls')),
     path('cart/', include('apps.orders.urls')),
     path('accounts/', include('apps.accounts.urls')),
+    # Standalone landing pages
+    path('fsc/', serve, {
+        'document_root': Path(settings.BASE_DIR) / 'fsc',
+        'path': 'index.html',
+    }),
     path('', include('apps.pages.urls')),
 ]
 
